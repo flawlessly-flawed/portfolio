632c3afa30a1aed44dad8bae857915f6cd20752c
ibraheem mariam <ibraheemmaryam26@gmail.com>
2024-05-18 05:04:09 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
+import About from './components/About'
+import Portfolio from './components/Portfolio'
 import Socials from './components/socials'
+
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
-
+      <About/>
+      <Portfolio/>
       <Socials/>
     </div>
   );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800
+     to-black text-white '>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+            justify-center w-full h-full'>
+            <div className='pb-8' >
+                <p className='text-4xl font-bold inline 
+                border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Excepturi praesentium ea maxime vitae blanditiis aliquam porro, 
+             accusamus inventore animi laborum officia odio! In, architecto. 
+             Provident blanditiis accusantium soluta eius harum labore fugiat 
+             debitis quaerat reprehenderit! Atque, qui expedita. Assumenda quia 
+             ipsam dolorem expedita id eligendi esse ipsum consequuntur, 
+             debitis neque?
+             </p>
+             <br />
+             <p className='text-xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 Inventore modi in iste nam similique doloribus debitis libero 
+                 deserunt molestiae nulla. Commodi voluptatibus incidunt 
+                 quisquam quos voluptatem iste! Dignissimos et, itaque modi h
+                 arum officia eveniet odit aliquid quasi placeat corrupti
+                  expedita vitae nulla est eum impedit ipsa molestias aut 
+                  corporis? Ipsa.</p>
+        </div>
+    </div>
   )
 }
 
